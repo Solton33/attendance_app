6dfbe6941e00b59fcb42a7810a88188d9287dccc
Solton33 <mid.night1202@gmail.com>
2026-03-29 19:06:32 +0900

    出勤打刻&退勤打刻処理
    出勤打刻の一部改修
    退勤打刻の作成

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,14 +1,23 @@
 class AttendancesController < ApplicationController
   before_action :set_setting
 
-  def index # 当日の内容を表示
+#################### 出退勤時刻の表示 ########################  
+
+  def index
     @attendance = Attendance.find_by(work_date:Date.today)
   end
 
-
+#################### 出勤処理 ########################
+  
   def clock_in
     # 今日の勤怠を取得 or 新規作成
     @attendance = Attendance.find_or_initialize_by(work_date: Date.today)
+
+    # 退勤打刻を既に打っている場合は出勤できないようにする
+    if @attendance.end_time.present?
+      flash[:alert] = "退勤打刻が設定済みです"
+      redirect_to root_path and return
+    end
 
     # 設定を紐づけ（activeがtrueのものを設定）
     @attendance.setting = @setting
@@ -28,43 +37,42 @@ class AttendancesController < ApplicationController
     redirect_to root_path
   end
 
+#################### 退勤処理 ########################
 
-  def clock_out # 退勤処理
+  def clock_out
     # 今日の勤怠を取得 or 新規作成
     @attendance = Attendance.find_or_initialize_by(work_date: Date.today)
-    
-    # 設定をがなければ紐づけ（activeがtrueのものを設定）
-    if @attendance.setting.nil?
-      @attendance.setting = @setting
-      @attendance.break_minutes = @setting.break_time
-    end
+          # 出勤打刻がないのでここで設定を紐づけ（activeがtrueのものを設定）
+      if @attendance.setting.nil? 
+        @attendance.setting = @setting
+      end
 
-    # 出勤打刻がない場合は、その旨のアラートを表示
-    if @attendance.start_time.nil?
-      flash[:alert] = "出勤打刻がされていません"
+    # 今日の日付がない = 出勤がされていないので警告後に退勤のみ打刻 
+    if @attendance.start_time.nil? && @attendance.end_time.nil?
+      flash[:alert] = "出勤打刻がされていません、後ほど修正してください"
 
+      @attendance.end_time = Time.current
+      @attendance.break_minutes ||= @setting.break_time
+      @attendance.save!
+      flash[:notice] = "退勤を打刻しました"
+
+    elsif @attendance.end_time.present?
+      flash[:alert] = "すでに退勤打刻済みです"
+
+      redirect_to root_path and return
+
+    else
       @attendance.end_time = Time.current
       @attendance.save!
       flash[:notice] = "退勤を打刻しました" 
-    
-    else
-      # 退勤日に退勤打刻があるかの確認
-      if @attendance.end_time.present?
-        flash[:alert] = "すでに退勤打刻済みです"
-        redirect_to root_path and return
-
-      else
-        # 退勤打刻をして保存
-        @attendance.end_time = Time.current
-        @attendance.save!
-        flash[:notice] = "退勤を打刻しました" 
-      end
     end
 
     # 打刻後にindex画面へ戻る
     redirect_to root_path
   end
+  
 
+#################### private処理 ########################
 
   private
 
