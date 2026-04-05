d74a5091ea247c722cfbf80aa9b192029759440b
Solton33 <mid.night1202@gmail.com>
2026-04-06 00:15:06 +0900

    出勤のmodelへの移行
    出勤処理のmodel以降
    Controller内の処理を一部modelへ移行

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,3 +1,26 @@
 class Attendance < ApplicationRecord
   belongs_to :setting
+
+  #################### 出勤処理 ########################
+  def clock_in(setting,now)
+    # 退勤打刻を既に打っている場合は出勤できないようにする
+    if end_time.present?
+      return ["退勤打刻が打刻済みです", false]
+    end
+
+    # 設定を紐づけ（activeがtrueのものを設定）
+    self.setting ||= setting
+
+    # 出勤日に出勤打刻があるかの確認
+    if start_time.nil?
+      self.start_time = now
+      self.break_minutes = setting.break_time
+      save!
+
+      return ["出勤を打刻しました", true]
+    else
+      return ["すでに出勤打刻済みです", false]
+    end
+  end
+
 end
