95d6b980142f9d9a55a462c404619b104bde42b9
Solton33 <mid.night1202@gmail.com>
2026-04-12 20:31:17 +0900

    CLフォルダへの対応
    CL関係のエラーへの対応

--- test/controllers/attendances_controller_test.rb
+++ test/controllers/attendances_controller_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
 class AttendancesControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    Setting.create!(active: true, break_time: 60)
+  end
+
   test "should get index" do
     get attendances_index_url
     assert_response :success
   end
+
 end
