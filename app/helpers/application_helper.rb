c79fcf08b5d2b35fd2523a3a83fe64b91ce20f6f
Solton33 <mid.night1202@gmail.com>
2026-05-03 20:10:25 +0900

    一覧表画面の調整および改修
    一覧表画面
    ・当月ボタンの追加
    ・当日の表示色を変更
    ・勤務時間を時分単位で表示
    ・勤務時間の未表示時の表示方法の設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def format_minutes(minutes)
+    if !(minutes)
+      return ""
+    end
+
+    hours = minutes / 60
+    mins = minutes % 60
+
+    "#{hours}:#{mins.to_s.rjust(2, '0')}"
+  end
 end
